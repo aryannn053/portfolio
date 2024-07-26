f318ed75bdc51528c7b7b96b9bb950111d250ea8
aryannn053 <aryannn053@gmail.com>
2024-07-26 19:44:12 +0530

    fix: vercel name issue

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ function App() {
           </p>
 
           <p className="whytho pt-6 text-gray-600">
-            A 13 year old doing competitive programming and frontend
+            A normal human doing competitive programming and frontend
             development. I've been doing competitive programming and web dev
             from the past 5 years. Welcome to my portfolio, here you will find
             everything I've done in my professional life.
