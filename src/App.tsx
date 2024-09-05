e7569d08e6dd0afa9a89195f2783dcc75d8c74b5
aryan <aryannn053@gmail.com>
2024-09-05 15:53:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ function App() {
           <p className="whytho pt-6 text-gray-600">
             A 14 year old doing competitive programming and frontend
             development. I've been doing competitive programming and web dev
-            from the past 5 years. Welcome to my portfolio, here you will find
+            from the past 6 years. Welcome to my portfolio, here you will find
             everything I've done in my professional life.
           </p>
 
