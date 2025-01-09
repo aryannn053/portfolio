455db8f14257ed8283a2a4103d4218d29d7dc667
aryan <aryannn053@gmail.com>
2025-01-09 18:25:17 +0530

    update: project and image error

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
           <div className="pt-1">
             <div className="experience-flex">
               <img
-                src="https://media.licdn.com/dms/image/D560BAQFj-7E-eQe8KA/company-logo_100_100/0/1721544337699/designmentorweb_logo?e=1730332800&v=beta&t=QLnekH8hh_BqJXwDXuODdY5d3Yd1VfxgEBUo2d7gTyg"
+                src="https://media.licdn.com/dms/image/v2/D560BAQFj-7E-eQe8KA/company-logo_100_100/company-logo_100_100/0/1721544337699/designmentorweb_logo?e=1744243200&v=beta&t=dQ4t2spK-9OzxnjmfJ2LBfDjy6RpCJy9zG2kd8JHQ-8"
                 className="rounded-full grayscale hover:grayscale-0"
                 width="50"
                 height="50"
@@ -327,6 +327,49 @@ function App() {
           <div className="px-10">
             <img
               src={image_3}
+              className="rounded grayscale hover:grayscale-0 pro-image"
+              width="400"
+            />
+          </div>
+        </div>
+        <div className="flexbox-grid-c flexbox-grid-c-cols-2 gap-0 pb-12">
+          <div>
+            <h1 className="wahre">• Dictionary App</h1>
+            <p className="whytho pt-1 text-gray-600">
+              A simple dictionary app that is written in Next JS which uses the Wiktionary API for information of a word.
+            </p>
+
+            <div className="flexbox-grid-c flexbox-grid-c-cols-2">
+              <a href="https://arytionary.vercel.app" className="wahatag pt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ionicon s-ion-icon"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M255.46 48.74c-114.84 0-208 93.11-208 208s93.12 208 208 208 208-93.12 208-208-93.15-208-208-208zM380.28 252c-2.85 32.63-16.79 49.7-28 58.26S327.61 322.58 316 320.5a41.61 41.61 0 01-26.82-17.19 62.06 62.06 0 01-44 17.57 51.66 51.66 0 01-38.55-16.83c-11.38-12.42-17-30.36-15.32-49.23 3-35 30.91-57.39 56.87-61.48 27.2-4.29 52.23 6.54 62.9 19.46l3.85 4.66-6.34 50.38c-1.19 14.34 3.28 23.48 12.29 25.1 2.39.42 8.1-.13 14.37-4.93 6.72-5.15 15.14-16 17.1-38.47 2.35-26.54-4.35-49.19-19.25-65.49-15.49-16.9-39.09-25.84-68.23-25.84-54 0-101.81 44.43-106.58 99-2.28 26.2 5.67 50.68 22.4 68.93 16.36 17.86 39.31 27.74 64.66 27.74 19 0 30.61-2.05 49.48-8.78a14 14 0 019.4 26.38c-21.82 7.77-36.68 10.4-58.88 10.4-33.28 0-63.57-13.06-85.3-36.77C138 321 127.42 288.94 130.4 254.82c2.91-33.33 18.45-64.63 43.77-88.12s57.57-36.49 90.7-36.49c37.2 0 67.93 12.08 88.87 34.93 20.09 21.91 29.51 52.75 26.54 86.86z"></path>
+                  <path d="M252.57 221c-14.83 2.33-31.56 15.84-33.34 36.26-1 11.06 2 21.22 8.07 27.87a23.65 23.65 0 0017.91 7.75c20.31 0 34.73-14.94 36.75-38.06a14 14 0 01.34-2.07l3.2-25.45a49.61 49.61 0 00-32.93-6.3z"></path>
+                </svg>
+                Try it now!
+              </a>
+
+              <a
+                href="https://github.com/aryannn053/dictionary"
+                className="wahatag pt-4"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ionicon s-ion-icon"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"></path>
+                </svg>
+                GitHub
+              </a>
+            </div>
+          </div>
+          <div className="px-10">
+            <img
+              src="https://pbs.twimg.com/media/Gg0YY3laMAAlrwr?format=png&name=900x900"
               className="rounded grayscale hover:grayscale-0 pro-image"
               width="400"
             />
